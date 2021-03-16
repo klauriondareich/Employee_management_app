667f2d318a0e49a15ec8cfefbaf58a51893831f2
Laurion <klauriondareich@gmail.com>
2021-03-16 15:55:49 -0700

    added login component route

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SignUpComponent} from './sign-up/sign-up.component'
+import {LoginComponent} from './login/login.component'
 
 const routes: Routes = [
-  {path: "sign_up",  component: SignUpComponent}
+  {path: "sign_up",  component: SignUpComponent},
+  {path: "login",  component: LoginComponent}
 ];
 
 @NgModule({
