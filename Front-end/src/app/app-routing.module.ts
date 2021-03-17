af0a0fdd72c49dc9d15b50add3a3245c025182e6
Laurion <klauriondareich@gmail.com>
2021-03-17 13:58:10 -0700

    added List all employees view route in app-routing.module.ts

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {SignUpComponent} from './sign-up/sign-up.component'
 import {LoginComponent} from './login/login.component'
+import {ListEmployeesComponent} from './list-employees/list-employees.component'
 
 const routes: Routes = [
   {path: "sign_up",  component: SignUpComponent},
-  {path: "login",  component: LoginComponent}
+  {path: "login",  component: LoginComponent},
+  {path: "employees",  component: ListEmployeesComponent},
 ];
 
 @NgModule({
