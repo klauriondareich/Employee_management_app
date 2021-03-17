067d55056312997c3eab182a21d719783231d647
Laurion <klauriondareich@gmail.com>
2021-03-17 16:10:25 -0700

    view-employee-details route added

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {SignUpComponent} from './sign-up/sign-up.component'
 import {LoginComponent} from './login/login.component'
 import {ListEmployeesComponent} from './list-employees/list-employees.component'
+import {ViewEmployeeDetailsComponent} from './view-employee-details/view-employee-details.component'
 
 const routes: Routes = [
   {path: "sign_up",  component: SignUpComponent},
   {path: "login",  component: LoginComponent},
   {path: "employees",  component: ListEmployeesComponent},
+  {path: "employees/:userId",  component: ViewEmployeeDetailsComponent},
 ];
 
 @NgModule({
