0c1baf4768137524dd98bb1de5f7cfaa591226bf
Laurion <klauriondareich@gmail.com>
2021-03-17 14:35:41 -0700

    new components navbar and sidebar created

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -7,14 +7,18 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import {FormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
-import { ListEmployeesComponent } from './list-employees/list-employees.component'
+import { ListEmployeesComponent } from './list-employees/list-employees.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
     LoginComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
