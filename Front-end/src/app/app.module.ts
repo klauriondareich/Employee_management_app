be2dfbfed844afa9861e9893e9524d5d9b0ebfb7
Laurion <klauriondareich@gmail.com>
2021-03-17 13:59:40 -0700

    i registered the ListEmployeesComponent in app.mdoule.ts

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import {FormsModule} from '@angular/forms'
 import {HttpClientModule} from '@angular/common/http';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { ListEmployeesComponent } from './list-employees/list-employees.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    LoginComponent
+    LoginComponent,
+    ListEmployeesComponent
   ],
   imports: [
     BrowserModule,
