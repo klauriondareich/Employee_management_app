4eefcf9640f814e1a615b8d18a5e9b4d25689aa1
laurion <klauriondareich@gmail.com>
2021-03-18 00:44:40 +0100

    impossible to login user due to unauthoriaztion request

--- Front-end/src/app/login.service.ts
+++ Front-end/src/app/login.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http"
+import { HttpClient } from "@angular/common/http"
+import { HttpHeaders } from "@angular/common/http"
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  url = "http://localhost:4000/config";
-
+  url = "http://127.0.0.1:5000/api/v1/login";
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: 'ImtsYXVyaW9uZGFyZWljaEBnbWFpbC5jb20i.YFKNZQ.xDfheRc661QcXvOE'
+    })
+  }
+  
   constructor( private http:HttpClient) { }
 
   // This func logins the user
   loginUser(data:object){
-    return this.http.post(this.url, data)
+    return this.http.post(this.url, data, this.httpOptions)
   }
 }
