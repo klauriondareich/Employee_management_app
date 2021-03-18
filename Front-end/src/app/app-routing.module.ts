98619a5845d6b240ec7d67a6802f68d9e7c41a72
laurion <klauriondareich@gmail.com>
2021-03-18 02:12:35 +0100

    added authGuard to routes which need users authentication

--- Front-end/src/app/app-routing.module.ts
+++ Front-end/src/app/app-routing.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {SignUpComponent} from './sign-up/sign-up.component'
-import {LoginComponent} from './login/login.component'
-import {ListEmployeesComponent} from './list-employees/list-employees.component'
-import {ViewEmployeeDetailsComponent} from './view-employee-details/view-employee-details.component'
-
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { ListEmployeesComponent } from './list-employees/list-employees.component';
+import { ViewEmployeeDetailsComponent } from './view-employee-details/view-employee-details.component';
+import { AuthGuardService as AuthGuard } from './auth-guard.service';
+ 
 const routes: Routes = [
   {path: "sign_up",  component: SignUpComponent},
   {path: "login",  component: LoginComponent},
-  {path: "employees",  component: ListEmployeesComponent},
-  {path: "employees/:userId",  component: ViewEmployeeDetailsComponent},
+  {
+    path: "employees",  
+    component: ListEmployeesComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "employees/:userId",  
+    component: ViewEmployeeDetailsComponent,
+    canActivate: [AuthGuard]
+  },
 ];
 
 @NgModule({
